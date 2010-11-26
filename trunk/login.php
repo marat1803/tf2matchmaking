b433ab295659ea7421cc187035a3c3ec3e83f0ce
stephen@costeira.com <stephen@costeira.com@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-11-26 22:51:51 +0000

    Removed double-login

--- trunk/login.php
+++ trunk/login.php
@@ -1,33 +1,51 @@
 <?php
+/*	#####################################
+	#				TF2MM				#
+	#									#
+	#	Login.php	-- R3				#
+	#									#
+	#	Stephen Costeira				#
+	#	11/26/10						#
+	#####################################
+	
+	-Need to look over this openid stuff below
+		--rearrange logic: ?login -- forward to openid provider
+		--it's all there, but in an odd way
+*/
 
-require_once 'includes/header.php';
-require 'openid.inc';
-try {
+
+require_once('includes/header.php');
+require_once('openid.inc');
+require_once('includes/steam_api.php');
+try{
     if(!isset($_GET['openid_mode'])) {
         if(isset($_GET['login'])) {
             $openid = new LightOpenID;
             $openid->identity = 'https://steamcommunity.com/openid';
             header('Location: ' . $openid->authUrl());
         }
-
-    } elseif($_GET['openid_mode'] == 'cancel') {
+    }elseif($_GET['openid_mode'] == 'cancel'){
         echo 'User has canceled authentication!';
-    } else {
+    }else{
         $openid = new LightOpenID;
 		$openid->validate();
 		$steamid = basename($openid->identity);
 		if(!$steamid) {
-			echo 'Auth failed';
-			exit;
+			die('Auth failed');
 		}
     }
-} catch(ErrorException $e) {
+}catch(ErrorException $e){
     echo $e->getMessage();
 }
 $steamid = basename($openid->identity);
 
-if (($steamid) == "") {
-echo '<style type="text/css">
+
+
+// </> End Review
+
+
+if($steamid == ""){
+	echo '<style type="text/css">
 .login {
   width:114px;
   height:43px;
@@ -39,34 +57,34 @@ echo '<style type="text/css">
 </style>
 <form action="?login" method="post">
     <button class="login">Login with Steam</button>
-</form>'; }
-else {
-$query = "SELECT * FROM users WHERE steamid = ". $steamid . " LIMIT 1 ";
-$result = mysql_query($query); 
-$row = mysql_fetch_assoc($result);
-
-if (isset($result) && ($steamid == $row[steamid]))
-		{
-				$_SESSION['steamid'] = $steamid;
-				header('Location: index.php');
-		} else {
-			if(isset($_POST['nickname'], $_POST['email'], $_POST['steamId64'])) {
-				$nickname = substr($_POST['nickname'], 0, 80);
-				$email = $_POST['email'];
-				$steamid = (string)$_POST['steamId64'];
-				$query = 'INSERT INTO `users` (`steamid`, `nickname`, `email`) VALUES(\'' .  mysql_real_escape_string($steamid) . '\', \'' .  mysql_real_escape_string($nickname) . '\', \'' .  mysql_real_escape_string($email) . '\');';
-				mysql_query($query) or die("Error in query: $result. ".mysql_error());
-				echo '<p>Welcome, ' . $nickname . '</p>'
-					.'<p>Your account has been successfully created.</p>';
-					$_SESSION['steamid'] = $steamid;
-			} else {
-				echo '<form method="post" action="">'
-					.'<input type="hidden" name="steamId64" value="' . $steamid . '">'
-					.'<input type="text" name="nickname"/>'
-					.'<input type="text" name="email">'
-					.'<input type="submit">'
-					.'</form>';
-			}
+</form>';
+}else{
+	//yay logged in through steam
+	
+	
+	
+	// 'HEY, THIS GUY LOGGED IN; HAS HE LOGGED IN BEFORE?'
+	$sql = 'SELECT * FROM `users` WHERE `steamid`="'.mysql_real_escape_string($steamid).'"'; //is escaping this necessary?
+	$result = mysql_query($sql);
+	$res = mysql_fetch_array($result);
+	//check
+	if($res['id'] != ''){ //YEAH HE WAS HERE BEFORE
+		echo 'Welcome back';
+		$_SESSION['steamid'] = $res;
+	}else{//NO MAN, SETUP NEW ACCOUNT
+	
+		//get information from Steam API
+			//this will get nickname and things
+		$xml = simplexml_load_file('http://steamcommunity.com/profiles/'.$steamid.'?xml=1');
+		//Insert info
+		if(!mysql_query("INSERT INTO `users` (steamid,nickname,online,country,regdate) VALUES ('".$steamid."','".$xml->steamID."','".$xml->onlineState."','".$xml->location."','".time()."')")){
+			die('Error: ' . mysql_error());
+		}else{
+			//everything went fine
+			header('Location: index.php');
 		}
+	}
 }
+
+
 ?>
