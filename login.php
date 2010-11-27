ec8da7780320b075f1bb8debc090e64b89b5cca3
stephen <stephen@costeira.com>
2010-11-27 16:32:50 +0000

    Reverted to older version. Added nicer info page.

--- login.php
+++ login.php
@@ -1,51 +1,33 @@
 <?php
-/*	#####################################
-	#				TF2MM				#
-	#									#
-	#	Login.php	-- R3				#
-	#									#
-	#	Stephen Costeira				#
-	#	11/26/10						#
-	#####################################
-	
-	-Need to look over this openid stuff below
-		--rearrange logic: ?login -- forward to openid provider
-		--it's all there, but in an odd way
-*/
-
-
-require_once('includes/header.php');
-require_once('openid.inc');
-require_once('includes/steam_api.php');
-try{
+//Maybe if I revert back to the original it'll be easier?
+require_once 'includes/header.php';
+require 'openid.inc';
+try {
     if(!isset($_GET['openid_mode'])) {
         if(isset($_GET['login'])) {
             $openid = new LightOpenID;
             $openid->identity = 'https://steamcommunity.com/openid';
             header('Location: ' . $openid->authUrl());
         }
-    }elseif($_GET['openid_mode'] == 'cancel'){
+
+    } elseif($_GET['openid_mode'] == 'cancel') {
         echo 'User has canceled authentication!';
-    }else{
+    } else {
         $openid = new LightOpenID;
 		$openid->validate();
 		$steamid = basename($openid->identity);
 		if(!$steamid) {
-			die('Auth failed');
+			echo 'Auth failed';
+			exit;
 		}
     }
-}catch(ErrorException $e){
+} catch(ErrorException $e) {
     echo $e->getMessage();
 }
 $steamid = basename($openid->identity);
 
-
-
-// </> End Review
-
-
-if($steamid == ""){
-	echo '<style type="text/css">
+if (($steamid) == "") {
+echo '<style type="text/css">
 .login {
   width:114px;
   height:43px;
@@ -57,34 +39,79 @@ if($steamid == ""){
 </style>
 <form action="?login" method="post">
     <button class="login">Login with Steam</button>
-</form>';
-}else{
-	//yay logged in through steam
+</form>'; }
+else {
+$query = "SELECT * FROM users WHERE steamid = ". $steamid . " LIMIT 1 ";
+$result = mysql_query($query); 
+$row = mysql_fetch_assoc($result);
+
+if (isset($result) && ($steamid == $row[steamid]))
+		{
+				$_SESSION['steamid'] = $steamid;
+				header('Location: index.php');
+		} else {
+			if(isset($_POST['nickname'], $_POST['email'], $_POST['steamId64'])) {
+				$nickname = substr($_POST['nickname'], 0, 80);
+				$email = $_POST['email'];
+				$steamid = (string)$_POST['steamId64'];
+				$query = "INSERT INTO `users` (`steamid`, `nickname`, `email`, `country`) VALUES('" .  mysql_real_escape_string($steamid) . "', '" .  mysql_real_escape_string($nickname) . "', '" .  mysql_real_escape_string($email) . "','".mysql_real_escape_string($_POST['loc'])."');";
+				mysql_query($query) or die("Error in query: $result. ".mysql_error());
+				echo '<p>Welcome, ' . $nickname . '</p>'
+					.'<p>Your account has been successfully created.</p>';
+					$_SESSION['steamid'] = $steamid;
+					
+					echo '<meta http-equiv="refresh" content="6;url=index.php">';
+			} else {
+				//get default information from Steam API
+				//this will get nickname and things
+			$xml = simplexml_load_file('http://steamcommunity.com/profiles/'.$steamid.'?xml=1');
 	
-	
-	
-	// 'HEY, THIS GUY LOGGED IN; HAS HE LOGGED IN BEFORE?'
-	$sql = 'SELECT * FROM `users` WHERE `steamid`="'.mysql_real_escape_string($steamid).'"'; //is escaping this necessary?
-	$result = mysql_query($sql);
-	$res = mysql_fetch_array($result);
-	//check
-	if($res['id'] != ''){ //YEAH HE WAS HERE BEFORE
-		echo 'Welcome back';
-		$_SESSION['steamid'] = $res;
-	}else{//NO MAN, SETUP NEW ACCOUNT
-	
-		//get information from Steam API
-			//this will get nickname and things
-		$xml = simplexml_load_file('http://steamcommunity.com/profiles/'.$steamid.'?xml=1');
-		//Insert info
-		if(!mysql_query("INSERT INTO `users` (steamid,nickname,online,country,regdate) VALUES ('".$steamid."','".$xml->steamID."','".$xml->onlineState."','".$xml->location."','".time()."')")){
-			die('Error: ' . mysql_error());
-		}else{
-			//everything went fine
-			header('Location: index.php');
+			echo '<style type="text/css">
+			fieldset{
+				margin:0px auto 0px auto;
+				width:500px;
+			}
+			input[type="text"]{
+				width:100%;
+			}
+			table{
+				margin-top:20px;
+			}
+			td{width:50%;vertical-align:top;}
+			tr td{
+				border-bottom:thin solid black;
+			}
+			tr:first-child td{border-top:thin solid black;}
+			tr:last-child td{
+				border-bottom:none;
+				text-align:center;
+			}
+			</style>
+			
+	<fieldset>
+	<legend>Welcome!</legend>
+	Thank you for using TF2MM! Since this is the first time you\'ve used TF2MM, we\'d like to know more about you. Entering this information is completely optional and not required, but will aid in better match making.
+	<form method="post" action="">
+	<input type="hidden" name="steamId64" value="' . $steamid . '">
+	<table>
+		<tr>
+			<td>Nickname:<br />(Cannot be blank)</td>
+			<td><input type="text" value="'.$xml->steamID.'" name="nickname"/></td>
+		</tr>
+		<tr>
+			<td>Email:<br />(Important messages and updates will be sent. Can opt-out at any time)</td>
+			<td><input type="text" name="email"/></td>
+		</tr>
+		<tr>
+			<td>Location</td>
+			<td><input type="text" name="loc" value="'.$xml->location.'" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" value="Submit" /></td>
+		</tr>
+	</table></form>
+	</fieldset>';
+			}
 		}
-	}
 }
-
-
 ?>
