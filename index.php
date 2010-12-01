dcc842c66720f8e117dc6183e99b61f9b1cf03f5
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-12-01 15:38:21 +0000

    theme.php and index.php merge

--- index.php
+++ index.php
@@ -1,23 +1,78 @@
 <?php
+
 require 'includes/header.php';
 
+if(isset($_SESSION['steamid'])) {
 
-if(isset($_SESSION['steamid']))
-{	echo '<img>' . $user->nickname . '<br />'
-. 'STEAM:' . GetAuthID($user->steamid) . '<br />'
-. 'main class: ' . '' . '<br />'
-. 'skill level: ' . '' . '<br />'
-. 'banpoints:' . $user->banpoints . '<br />'
-. 'rating: ' . $user->rating . '<br />'
-; 
-echo "<br /> <br />";
-echo 'LOBBY LAWL:<br />' .$lobby->name .'<br />'
-. 'Region:' .$lobby->region .'<br />'
-. 'Players:' .$lobby->players .'<br />'
-. 'Rules:' .$lobby->rules .'<br />'
-. 'Status:' .$lobby->status .'<br />'
-;
+$id = displayID($_SESSION['steamid']);
+
+		
+echo '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title>TF2 Matchmaking System</title>
+	<link href="theme/style.css" rel="stylesheet" type="text/css" />
+	<link href="theme/uniform.default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="background_image"></div>
+	<div id="wrapper">
+		<div id="header">
+			<a href="" id="logo">TF2 Matchmaking System</a>
+		</div>
+		<ul id="sidebar">
+			<li class="play_now_button">Play Now!</li>
+			<li id="lobby_filter">
+				<ul id="lobbys_opened">
+					<li>25 open lobbys</li>
+					<li>23 lobbys in play</li>
+				</ul>
+				<form method="get" action="">
+					<input type="checkbox" value="true" id="not_full"><label for="not_full">Not Full</label>
+					<div class="clear"></div>
+					<select id="skill_from">
+						<option>Division 1</option>
+						<option>Division 2</option>
+						<option>Division 3</option>
+						<option>Division 4</option>
+						<option>Division 5</option>
+						<option>Division 6</option>
+					</select>
+					<select id="skill_to">
+						<option>Division 1</option>
+						<option>Division 2</option>
+						<option>Division 3</option>
+						<option>Division 4</option>
+						<option>Division 5</option>
+						<option>Division 6</option>
+					</select>
+				</form>
+			</li>
+			<li class="profile_panel">';
+			echo $user->mainuser($id) .'
+			</li>
+			<li class="friends_panel">
+				<h1>Friends</h1>
+				<ul>';
+				foreach(queryfr($user->id) as $friend) {
+				echo $friend;
+				} echo '
+				</ul>
+			</li>
+		</ul>
+		<div id="content">
+			<ul id="lobby_list">';
+				echo $lobby->displaylobby($id).'
+			</ul>
+		</div>
+	</div>
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/jquery.uniform.min.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+</body>
+</html>
+';
 }
 else header('Location: login.php');
-
 ?>
