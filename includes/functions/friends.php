05e54bc4cce4e258d0b33f3d984853ccb1a20992
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-12-05 17:43:11 +0000

    lastseen and also getonline rewrite (ty obstipator)

--- includes/functions/friends.php
+++ includes/functions/friends.php
@@ -1,14 +1,15 @@
 <?php
 
-function getStatus ($id)
+function getOnline ($id)
 {
-	$query = 'SELECT online FROM users WHERE id = '.$id;
-	$result = mysql_query($query);
-	$statuss = mysql_fetch_assoc($result);
-	if ($statuss['online'] == 1) $status = "Online";
-		else $status = "Offline";
-	return $status;
+	$sql = 'SELECT * FROM users WHERE id = '.$id;
+	$query = mysql_query($query);
+	$users = mysql_fetch_assoc($result);
+	$lastseen = $query['lastseen'];
+	if (time() - strtotime($lastseen) > 5 * 60)) return "Offline";
+	else return "Online";
 }
+
 
 function getfriends ($id) {
 	$query = 'SELECT friends FROM users WHERE id='.$id;
@@ -18,19 +19,20 @@ function getfriends ($id) {
 	if ($fids != "") {
 	$fidsar = explode(",", $fids);
 		foreach($fidsar as $fid) {
-			$query = 'SELECT * FROM users WHERE id=' . $fid.' ORDER BY online DESC';
+			$online = getOnline($fid);
+			$query = 'SELECT * FROM users WHERE id=' . $fid.' ORDER BY lastseen DESC';
 			$result = mysql_query($query);
 			$friendsinfo = mysql_fetch_assoc($result);
 			$nickname = $friendsinfo['nickname'];
 			$steamid = $friendsinfo['steamid'];
 			$uid = $friendsinfo['id'];
-			if ($friendsinfo['online'] == 1) $status = '<li>';
+			if ($online == "Online") $status = '<li>';
 				else $status = '<li class="friend_offline">';
 			$return = $status.'
 			<img src='.APIGet($steamid,avatar).'></img>'.
 			'<span class="user_name">'.$nickname.'</span>'.
 			'<span class="user_steamid">'.GetAuthID($steamid).'</span>'.
-			'<span class="user_steamon">'.getStatus($uid).'</span><br />
+			'<span class="user_steamon">'.$online.'</span><br />
 			</li>';
 			echo $return;
 		}
