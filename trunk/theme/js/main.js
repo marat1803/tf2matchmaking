9262c2427866333a7eda7d5dd76c2cefb2798407
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-12-05 12:04:42 +0000

    toggle finally work props to frigginSnowedIn @ #jquery

--- trunk/theme/js/main.js
+++ trunk/theme/js/main.js
@@ -1,6 +1,9 @@
  $(document).ready(function(){
 	$("#skill_from, #skill_to, #not_full").uniform();
-	function lid(lid) {
-	$('#lobby_tooltip:' + lid).toggle();
-	};
- });
+	$(".lobby_tooltip").hide();
+	(function($) {
+    $("li.lobby_panel").click(function(){
+        $('#'+$(this).attr('data-panel').replace('-', '\\:')).toggle();
+    });
+	})(jQuery);
+});
