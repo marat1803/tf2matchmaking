60c1ac57f0baa85ef331c770eb99e67f8e767f1f
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-12-09 18:29:18 +0000

    etf2l api

--- trunk/includes/classes/class.user.php
+++ trunk/includes/classes/class.user.php
@@ -3,6 +3,7 @@
 require_once('includes/functions/rating.php');
 require_once('includes/functions/friends.php');
 require_once('includes/functions/steamcomm.php');
+require_once('includes/functions/etf2l.php');
 
 function displayID($steamid) {
 	$sql =  'SELECT * FROM users WHERE steamid= \'' . $steamid . '\' LIMIT 1';
@@ -41,8 +42,10 @@ class user {
 	$this->id		= $userinfo['id'];
 	$this->nickname = $userinfo['nickname'];
 	$this->steamid  = $userinfo['steamid'];
+	$this->steamid = $userinfo['steamid'];
 	$this->rating 	= rating($this->id);
 	$this->avatar 	= APIGet($this->steamid,avatar);
+	$this->etf2lid = str_replace(steam, "", GetAuthID($this->steamid));
 	
 	return  '
 	            <div class="profile_panel">
@@ -51,7 +54,7 @@ class user {
                 <span class="user_steamid">'.GetAuthID($this->steamid).'</span>
                 <dl>
                     <dt>Mainclass:</dt><dd><img style="float: left;" class="class_icon" src="theme/images/class/demo.png" height="14"><span style="float: left; margin-left: 3px;">Demoman</span></dd>
-                    <dt>Skilllevel:</dt><dd>Division 4</dd>
+                    <dt>Skilllevel:</dt><dd>Division '.etf2ldiv($this->etf2lid).'</dd>
                     <dt>Rating:</dt><dd>'.$this->rating.'</dd>
                 </dl>
             </div>';
--- trunk/profile.php
+++ trunk/profile.php
@@ -50,49 +50,5 @@ if($steamid != $_SESSION['steamid']) {
 </html>';
 
 
-/*
-$steamid = '0:1:16096891';
-
-function objectsIntoArray($arrObjData, $arrSkipIndices = array())
-{
-    $arrData = array();
-    
-    // if input is object, convert into array
-    if (is_object($arrObjData)) {
-        $arrObjData = get_object_vars($arrObjData);
-    }
-    
-    if (is_array($arrObjData)) {
-        foreach ($arrObjData as $index => $value) {
-            if (is_object($value) || is_array($value)) {
-                $value = objectsIntoArray($value, $arrSkipIndices); // recursive call
-            }
-            if (in_array($index, $arrSkipIndices)) {
-                continue;
-            }
-            $arrData[$index] = $value;
-        }
-    }
-    return $arrData;
-}
-
-
-
-$xmlUrl = 'http://etf2l.org/feed/player/?steamid='.$steamid; // XML feed file/URL
-$xmlStr = file_get_contents($xmlUrl);
-$xmlObj = simplexml_load_string($xmlStr);
-$arrXml = objectsIntoArray($xmlObj);
-
-		
-foreach($arrXml['player']['teams']['team'] as $team) {
-	if($team['@attributes']['type'] != '6on6') {
-		continue;
-	}
-	foreach($team['comp'] as $comp) {
-			echo preg_replace('#^Division#', '', $comp['@attributes']['division']).'<br/>'; }
-	}
-//	echo $team['@attributes']['name'].'<br/>'; */
-
-
 	
 ?>
