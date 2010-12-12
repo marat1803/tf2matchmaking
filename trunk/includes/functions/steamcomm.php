68fa4e4b88a5abe839aaacb84e236afe2f6dbc37
isharis@gmail.com <isharis@gmail.com@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-12-12 18:38:44 +0000

    Added cache for steam images

--- trunk/includes/functions/steamcomm.php
+++ trunk/includes/functions/steamcomm.php
@@ -17,10 +17,23 @@ function GetAuthID($i64friendID)
 
 function APIGet ($steamid,$what)
 {
-$api = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key=8DEE586C5830B73298B0C2F06D330E3C&steamids='. $steamid);
-$result = json_decode($api);
-
-
-return $result->response->players->player[0]->$what;
+	if($what == 'avatar') {
+		clearstatcache();
+		if(!file_exists('cache/avatars/'.$steamid.'.jpg')) {
+			$api = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key=8DEE586C5830B73298B0C2F06D330E3C&steamids='. $steamid);
+			$result = json_decode($api);
+			$avatar = $result->response->players->player[0]->$what;
+			$filename = 'cache/avatars/'.$steamid.'.jpg';
+			copy($avatar, $filename);
+			
+			return 'cache/avatars/'.$steamid.'.jpg';
+		} else {
+			return 'cache/avatars/'.$steamid.'.jpg';
+		}
+	} else {
+		$api = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0001/?key=8DEE586C5830B73298B0C2F06D330E3C&steamids='. $steamid);
+		$result = json_decode($api);
+		return $result->response->players->player[0]->$what;
+	}
 }
 ?>
