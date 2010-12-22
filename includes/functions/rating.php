97b58bf81b92aebef55ca14f6067d78c300394fb
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2010-12-22 13:25:09 +0000

    rate player function

--- includes/functions/rating.php
+++ includes/functions/rating.php
@@ -19,4 +19,31 @@ function rating($id)
 	elseif ($ratingscore < 0.2) return "---";
 }
 
+function rate($source,$target,$value) {
+	$sql = 'SELECT rated FROM lobby_players WHERE id = '.mysql_real_escape_string($source);
+	$query = mysql_query($sql);
+	$rated = mysql_fetch_assoc($query);
+	$rated = $rated['rated'];
+	$rates = explode(',', $rated);
+	foreach ($rates as $rate) {
+		if ($rate != $target) $allow = true;
+		else $allow = false;
+	}
+	if ($allow) {
+			if ($rated == "") $rated .= $target; 
+            else $rated .= ','.$target;
+            $rated = mysql_real_escape_string($rated);
+			$sql = 'UPDATE lobby_players SET rated = \''.$rated.'\' WHERE id = '.$source;
+			mysql_query($sql); 
+			if ($value == 1) {
+				$sql = 'UPDATE ratings SET plus = plus + 1 WHERE id = '.$target;		
+			} else {
+				$sql = 'UPDATE ratings SET minus = minus + 1 WHERE id = '.$target;
+			}
+			mysql_query($sql);
+	} else echo 'You already rated this player';
+}
+
+rate(4,3,1);
+
 ?>
