84b898b6af0fa9f15d036769830cf222e226a5e5
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2011-01-10 22:40:10 +0000

    valve-like hotfix

--- includes/functions/etf2l.php
+++ includes/functions/etf2l.php
@@ -3,23 +3,24 @@
 
 function etf2ldiv($etf2lsteamid) {
 
-$xmlUrl = 'http://etf2l.org/feed/player/?steamid='.$etf2lsteamid; // XML feed file/URL
-$xmlStr = file_get_contents($xmlUrl);
-$api = new SimpleXMLElement($xmlStr);
+    $xmlUrl = 'http://etf2l.org/feed/player/?steamid='.$etf2lsteamid; // XML feed file/URL
+    $xmlStr = file_get_contents($xmlUrl);
+    $api = new SimpleXMLElement($xmlStr);
 
 
-if(isset($api->player)) {
-    foreach ($api->player->teams->team as $team) {
-        if (isset($team->comp) && $team->attributes()->type == '6on6') {
-            $last_comp = $team->comp[sizeof($team->comp) - 1];
-                if(isset($last_comp->attributes()->division)) {
-                    $division = $last_comp->attributes()->division;
-                    if ($division == 'Premier Division') return 'Premier';
-                    else {
-                        $divnumber = preg_replace('#Division#', '', $division);
-                        return substr($divnumber,  0, -1);
+    if(isset($api->player)) {
+        foreach ($api->player->teams->team as $team) {
+            if (isset($team->comp) && $team->attributes()->type == '6on6') {
+                $last_comp = $team->comp[sizeof($team->comp) - 1];
+                    if(isset($last_comp->attributes()->division)) {
+                        $division = $last_comp->attributes()->division;
+                        if ($division == 'Premier Division') return 'Premier';
+                        else {
+                            $divnumber = preg_replace('#Division#', '', $division);
+                            return substr($divnumber,  0, -1);
+                        }
                     }
-                }
+            }
         }
     }
 }
