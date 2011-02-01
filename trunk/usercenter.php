db7c97182fdf7759f61edbb8b2201d26087205b0
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2011-02-01 21:37:25 +0000

    forgot to add usercenter.php

---
+++ trunk/usercenter.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'includes/header.php';
+
+$css = 'style_profile.css';
+
+$id = $_SESSION['id'];
+
+if ($id) {
+	$user = new User($id);
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if ($_POST['nickname'] != $user->nickname) $details['nickname'] = $_POST['nickname'];
+		if ($_POST['email'] != $user->email) $details['email'] = $_POST['email'];
+		if ($_POST['loc'] != $user->country) $details['country'] = $_POST['loc'];
+		$user->updateDetails($details);
+		$user = new User($id);
+	}
+
+	include_once 'includes/header.inc';
+	include_once 'includes/pages/usercenter.inc';
+
+
+} else header('Location: index.php');
+
+?>
