954a53bc1dfa985bb4ecf2f436d7d8cde5dd4889
alexandrualexandu <alexandrualexandu@ecea6ace-3e49-7127-1b75-81bb55a3b0c0>
2011-02-20 12:15:26 +0000

    admin.php

---
+++ admin.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'includes/header.php';
+$css = 'style_profile.css';
+include_once 'includes/header.inc';
+
+
+$id = $_SESSION['id'];
+$post = false;
+
+if ($id) $user = new User($id);
+
+if ($user->status == "admin") {
+	switch ($_GET['page']) {
+		case "banuser":
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+				$post = true;
+				if (isset($_POST['user']) && isset($_POST['reason']) && isset($_POST['lobby'])) {
+					if ($_POST['reason'] == 'custom') {
+						if (isset($_POST['comment']) && isset($_POST['amount'])) $message = newBanPoints($_POST['user'],$_POST['lobby'],$id,$_POST['reason'],$_POST['amount'],$_POST['comment']);
+						else $error = 'Both comment and amount field must be filled';
+					} else $message = newBanPoints($_POST['user'],$_POST['lobby'],$id,$_POST['reason'],$_POST['amount'],$_POST['comment']);
+				}
+				include_once 'includes/pages/adminban.inc';
+			} else include_once 'includes/pages/adminban.inc';
+	}
+} else header('Location: index.php');
+
+
+?>
